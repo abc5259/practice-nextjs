20ec1687d23de3626c7f0b43c70c8105981a9506
LeeJaeHoon <dlwogns3413@naver.com>
2022-03-04 01:08:37 +0900

    Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,47 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 const API_KEY = "276ed8bd5e4b119ec47398e95723094b";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <>
       <Seo title="home" />
       <div className="container">
         {results?.map(movie => (
-          <div className="movie" key={movie.id}>
+          <div
+            onClick={() => onClick(movie.id, movie.original_title)}
+            key={movie.id}
+            className="movie"
+          >
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-            <h4>{movie.original_title}</h4>
+            <h4>
+              <Link
+                href={{
+                  pathname: `/movies/${movie.id}`,
+                  query: {
+                    title: movie.original_title,
+                  },
+                }}
+                as={`/movies/${movie.id}`}
+              >
+                <a>{movie.original_title}</a>
+              </Link>
+            </h4>
           </div>
         ))}
       </div>
@@ -21,6 +51,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
